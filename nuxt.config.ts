07bd1270fd6d6a8aab2740f02cb858979da66258
김철환(Louis) - 커머스사업2그룹 <louis@shinsegae.com>
2024-07-15 17:56:34 +0900

    Firebase 설정 추가 - npm install 필요

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,18 @@ export default defineNuxtConfig({
     strict: true
   },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  plugins: [
+    '~/plugins/firebase.ts',
+  ],
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    }
+  },
 })
--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,23 +1,33 @@
-// import { initializeApp } from 'firebase/app'
-// import { getAuth } from 'firebase/auth'
-// import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { useRuntimeConfig } from "#app";
 
-// Firebase 설정
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+interface FirebaseConfig {
+  apiKey : string;
+  authDomain : string;
+  projectId: string;
+  storageBucket : string;
+  messagingSenderId : string;
+  appId : string
 }
 
-// Firebase 초기화
-// const app = initializeApp(firebaseConfig)
-const app = '';
-// Firebase 인증과 Firestore 서비스 초기화
-// const auth = getAuth(app)
-const auth = '';
-// const firestore = getFirestore(app)
-const firestore = '';
-export { auth, firestore }
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+  const firebaseConfig : FirebaseConfig = {
+    apiKey: config.public.firebaseApiKey,
+    authDomain: config.public.firebaseAuthDomain,
+    projectId: config.public.firebaseProjectId,
+    storageBucket: config.public.firebaseStorageBucket,
+    messagingSenderId: config.public.firebaseMessagingSenderId,
+    appId: config.public.firebaseAppId,
+  };
+
+  // Firebase 초기화
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  nuxtApp.provide('auth', auth);
+  nuxtApp.provide('db', db);
+});
